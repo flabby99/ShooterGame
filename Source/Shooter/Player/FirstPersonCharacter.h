3fd7bb6aa93b38a363582463945872dd83e093d5
Sean Martin <martins7@tcd.ie>
2017-08-03 13:12:35 +0100

    Refactored cpp structure

--- Source/Shooter/Player/FirstPersonCharacter.h
+++ Source/Shooter/Player/FirstPersonCharacter.h
@@ -4,12 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "ShooterCharacter.generated.h"
+#include "FirstPersonCharacter.generated.h"
 
 class UInputComponent;
 
 UCLASS(config=Game)
-class AShooterCharacter : public ACharacter
+class AFirstPersonCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
@@ -46,7 +46,7 @@ class AShooterCharacter : public ACharacter
 	class UMotionControllerComponent* L_MotionController;
 
 public:
-	AShooterCharacter();
+	AFirstPersonCharacter();
 
 protected:
 	virtual void BeginPlay();
@@ -66,7 +66,7 @@ public:
 
 	/** Projectile class to spawn */
 	UPROPERTY(EditDefaultsOnly, Category=Projectile)
-	TSubclassOf<class AShooterProjectile> ProjectileClass;
+	TSubclassOf<class ABallProjectile> ProjectileClass;
 
 	/** Sound to play each time we fire */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
--- Source/Shooter/Weapons/BallProjectile.h
+++ Source/Shooter/Weapons/BallProjectile.h
@@ -4,10 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "ShooterProjectile.generated.h"
+#include "BallProjectile.generated.h"
 
 UCLASS(config=Game)
-class AShooterProjectile : public AActor
+class ABallProjectile : public AActor
 {
 	GENERATED_BODY()
 
@@ -20,7 +20,7 @@ class AShooterProjectile : public AActor
 	class UProjectileMovementComponent* ProjectileMovement;
 
 public:
-	AShooterProjectile();
+	ABallProjectile();
 
 	/** called when projectile hits something */
 	UFUNCTION()
